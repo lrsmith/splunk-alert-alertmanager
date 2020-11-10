b38547f15f0fdf2bacba7b0f8dc351c63fff7163
Len Smith <lrsmith@umich.edu>
2020-11-09 19:34:22 -0800

    feat: Updated and documented inline payload data

--- bin/alertmanager.py
+++ bin/alertmanager.py
@@ -6,18 +6,20 @@ OK = 0
 ERROR_CODE_UNKNOWN = 1
 ERROR_CODE_VALIDATION_FAILED = 2
 
+# This is the JSON payload that Alertmanager expects to see. This will
+#  be populated by data from the Splunk Alert Payload.
 ALERT_MANAGER_MSG = {
   'status' : 'firing',
   'labels' : {
-      'alertname' : 'alertname',
-      'service'   : 'splunk',
-      'severity'  : 'warning',
-      'instance'  : 'instance',
+      'alertname'    : '',                  # Populate with Splunk Search Name
+      'job'          : 'splunk',            
+      'severity'     : 'critical',
+      'instance'     : '',                  # Populate with Splunk Server Host
   },
-  'annotations'   : {
-      'summary'   : 'Annotation Summary'
+  'annotations'      : {
+      'summary'      : 'Annotation Summary'
   },
-  'generatorURL'  : 'http://prometheus.int.example.net/<generating_expression>'
+  'generatorURL'  : ''                      # Populate with Splunk server and search URI
 }
 
 def log(msg, *args):
@@ -38,7 +40,12 @@ def valdidate_payload(payload):
     return True
 
 def build_alertmanager_message(payload):
-    ALERT_MANAGER_MSG['labels']['alertname'] = payload['search_name'] 
+    ALERT_MANAGER_MSG['labels']['alertname']         = payload['search_name'] 
+    ALERT_MANAGER_MSG['labels']['instance']          = payload['server_host'] 
+
+    # Shows up as 'source' in alert manager and takes you to Splunk Search
+    ALERT_MANAGER_MSG['generatorURL']                = payload['server_uri'] + payload['search_uri'] 
+
     return ALERT_MANAGER_MSG
 
 if __name__ == '__main__':
@@ -49,7 +56,7 @@ if __name__ == '__main__':
     alert_payload=build_alertmanager_message(payload)
 
     config = payload.get('configuration')
-    log("INFO Sending alert payload")
+    #log("INFO Sending alert payload")
     res = requests.post(config.get('alertmanager_url'), json=[alert_payload])
     
 
