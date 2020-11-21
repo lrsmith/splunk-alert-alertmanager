09fdeb55f155f3cb1f763c0eb63f97fc7b20d24b
Len Smith <lrsmith@umich.edu>
2020-11-20 20:40:55 -0800

    feat: Break out alert by specified splunk fields (#2)

--- app/bin/alertmanager.py
+++ app/bin/alertmanager.py
@@ -1,6 +1,9 @@
 import json
 import sys
 import requests
+import gzip
+import csv
+import copy
 
 OK = 0
 ERROR_CODE_UNKNOWN = 1
@@ -40,25 +43,52 @@ def valdidate_payload(payload):
     return True
 
 def build_alertmanager_message(payload):
+    # Set 'global'
     ALERT_MANAGER_MSG['labels']['alertname']         = payload['search_name'] 
     ALERT_MANAGER_MSG['labels']['instance']          = payload['server_host'] 
-
-    # Shows up as 'source' in alert manager and takes you to Splunk Search
+        # Shows up as 'source' in alert manager and takes you to Splunk Search
     ALERT_MANAGER_MSG['generatorURL']                = payload['server_uri'] + payload['search_uri'] 
-
     ALERT_MANAGER_MSG['labels']['severity']          = payload['configuration']['severity'] 
 
-    return ALERT_MANAGER_MSG
+    alerts = []
+
+    # Split out alert(s) based on fields. i.e. cell or cell,customer
+    if payload['configuration'].has_key('group_by'):
+
+        log("INFO : Grouping alerts by %s." % payload['configuration']['group_by'])
+        results = set()
+
+        with gzip.open(payload.get('results_file'),'rt') as f:
+            csv_dict = csv.DictReader(f)
+            for row in csv_dict:
+                tmp_hash = {}
+                for field in set(payload['configuration']['group_by'].split(",")):
+                    tmp_hash[field] = row[field]
+                results.add(json.dumps(tmp_hash))
+
+        # For each result generate an alert
+        for tup in results:
+            tmp_alert = copy.deepcopy(ALERT_MANAGER_MSG) 
+            tmp_hash = json.loads(tup)
+            for key in tmp_hash:
+                tmp_alert['labels'][key] = tmp_hash[key]
+            alerts.append(tmp_alert)
+
+    else:
+        alerts.append(ALERT_MANAGER_MSG)
+
+    return alerts 
 
 if __name__ == '__main__':
+
     payload = json.loads(sys.stdin.read())
 
     if not valdidate_payload(payload):
         sys.exit(ERROR_CODE_VALIDATION_FAILED)
-    alert_payload=build_alertmanager_message(payload)
 
     config = payload.get('configuration')
-    log("INFO Sending alert payload %s " % payload)
-    res = requests.post(config.get('alertmanager_url'), json=[alert_payload])
-    
+
+    alert_payload=build_alertmanager_message(payload)
+    res = requests.post(config.get('alertmanager_url'), json=alert_payload)
+
 
